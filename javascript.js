21b0e909f5ef27cefe0d47b71f2b44bbf7db0198
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-05 14:26:38 -0700

    Basic

--- javascript.js
+++ javascript.js
@@ -1,17 +1,59 @@
-
-function displayResult() {
-  document.getElementById("textmessage").innerHTML = "Have a nice day!";
-  document.getElementById("bodypage").style.backgroundColor = "yellow";
+document.getElementById("btn").addEventListener("click", function(){
+  alert('button clicked');
 }
 
-function retrieveTextFunction(){
-  return
+function onPageLoads(){
+  loadFileFunction();
 }
 
-function submitButtonReactivate(){
-
+function testReturnText(){
+  document.getElementById('textmessage').innerText = 'it works';
 }
 
-function submittedNotification(){
-  alert("Submitted!");
+function saveFileFunction(){
+  const fileName = 'mytext.txt';
+  const data = document.querySelector("#textmessage").value;
+
+  saveFile(fileName, data, function(err){
+    if (err) {
+      alert('Failed to save: ' + fileName + '\n' + err);
+    } else {
+      alert('Saved ' + fileName);
+    }
+  });
+}
+
+function loadFileFunction(){
+  const fileName = 'mytext.txt';
+
+  loadFile(fileName, function (err, data){
+    if (err) {
+      alert('Failed to load: ' + fileName + '\n' + err);
+    } else {
+      document.querySelector("#textmessage").value = data;
+      alert('Loaded ');
+    }
+  })
+}
+
+function saveFile(filename, data, callback) {
+    doXhr(filename, 'PUT', data, callback);
+}
+
+function loadFile(filename, callback) {
+    doXhr(filename, 'GET', '', callback);
+}
+
+//helper method
+function doXhr(url, method, data, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+          callback(null, xhr.responseText);
+      }  else {
+          callback('Request failed.  Returned status of ' + xhr.status);
+      }
+  };
+  xhr.send(data);
 }
