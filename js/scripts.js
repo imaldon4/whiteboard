4b48933546b06b7ca4d1fb61a271021fdf53509b
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-05 16:39:13 -0700

    lots

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today is + " + d + "</h1>";
+
+
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
