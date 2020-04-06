fc586ec45d77b0c1e9213bac29fb430698e58a3e
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-04-05 17:03:20 -0700

    reverting

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+function displayResult() {
+  document.getElementById("textmessage").innerHTML = "Have a nice day!";
+  document.getElementById("bodypage").style.backgroundColor = "yellow";
+}
+
+function retrieveTextFunction(){
+  return
+}
+
+function submitButtonReactivate(){
+
+}
+
+function submittedNotification(){
+  alert("Submitted!");
+}
